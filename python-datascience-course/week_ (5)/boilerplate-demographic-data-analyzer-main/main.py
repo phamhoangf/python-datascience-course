2d1a5d54c2cc6de2657087e7068ea4b24827b746
phamhoangf <125198269+phamhoangf@users.noreply.github.com>
2023-09-12 22:11:58 +0700

    'remove

--- python-datascience-course/week_ (5)/boilerplate-demographic-data-analyzer-main/main.py
+++
@@ -1,8 +0,0 @@
-import demographic_data_analyzer
-from unittest import main
-
-# Test your function by calling it here
-demographic_data_analyzer.calculate_demographic_data()
-
-# Run unit tests automatically
-main(module='test_module', exit=False)
--- python-datascience-course/week_ (5)/boilerplate-mean-variance-standard-deviation-calculator-main/mean_var_std.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def calculate(list):
-    return calculations
